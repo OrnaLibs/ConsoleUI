846988c906ebc99cb53c189f31de4f6839b9cef8
OrnarasUS <ornaras.us@gmail.com>
2025-01-28 17:47:37 +0400

    Реализован Label

---
+++ Builders/LabelBuilder.cs
@@ -0,0 +1,51 @@
+﻿using OrnaLibs.ConsoleUI.Abstractions;
+using System;
+using System.Text;
+
+namespace OrnaLibs.ConsoleUI.Builders
+{
+    public sealed class LabelBuilder : Builder<LabelBuilder, Label>
+    {
+        private StringBuilder _text = new StringBuilder();
+        private ConsoleColor _fg = Console.ForegroundColor;
+        private ConsoleColor _bg = Console.BackgroundColor;
+
+        protected internal override LabelBuilder GetSelf() => this;
+
+        public LabelBuilder BackgroundColor(ConsoleColor color)
+        {
+            _bg = color;
+            return GetSelf();
+        }
+
+        public LabelBuilder ForegroundColor(ConsoleColor color)
+        {
+            _fg = color;
+            return GetSelf();
+        }
+
+        public LabelBuilder Text(string text)
+        {
+            _text.Append(text);
+            return GetSelf();
+        }
+
+        public LabelBuilder Format(string key, object value)
+        {
+            _text.Replace(key, value.ToString());
+            return GetSelf();
+        }
+
+        public LabelBuilder Line(string text)
+        {
+            _text.AppendLine(text);
+            return GetSelf();
+        }
+
+        public override Label Build() =>
+            new Label(_text.ToString(), _bg, _fg)
+            {
+                OnCancel = onCancel
+            };
+    }
+}
---
+++ Label.cs
@@ -0,0 +1,30 @@
+﻿using OrnaLibs.ConsoleUI.Abstractions;
+using System;
+
+namespace OrnaLibs.ConsoleUI
+{
+    public sealed class Label : Control
+    {
+        private string _text;
+        private ConsoleColor _background;
+        private ConsoleColor _foreground;
+
+        internal Label(string text, ConsoleColor bgColor, ConsoleColor fgColor)
+        {
+            _text = text;
+            _background = bgColor;
+            _foreground = fgColor;
+        }
+
+        protected internal override void OnPressed(ConsoleKeyInfo key) { }
+
+        protected internal override void Render()
+        {
+            Console.Clear();
+            Console.ForegroundColor = _foreground;
+            Console.BackgroundColor = _background;
+            Console.WriteLine(_text);
+            Console.ResetColor();
+        }
+    }
+}
